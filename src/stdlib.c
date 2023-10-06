63318d5667bf7311e81e9614be8e7cd5a4da2da5
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-10-06 00:18:06 -0700

    remove plists & don't create them anymore

--- src/stdlib.c
+++ src/stdlib.c
@@ -43,7 +43,7 @@ int p101_abs(const struct p101_env *env, __attribute__((unused)) struct p101_err
     return ret_val;
 }
 
-int p101_atexit(const struct p101_env *env, struct p101_error *err, void (*func)(void))    // cppcheck-suppress constParameterPointer
+int p101_atexit(const struct p101_env *env, struct p101_error *err, void (*func)(void))
 {
     int ret_val;
 
@@ -60,8 +60,7 @@ int p101_atexit(const struct p101_env *env, struct p101_error *err, void (*func)
     return ret_val;
 }
 
-void *p101_bsearch(const struct p101_env *env, const void *key, const void *base, size_t nel, size_t width,
-                   int (*compar)(const void *, const void *))    // cppcheck-suppress constParameterPointer
+void *p101_bsearch(const struct p101_env *env, const void *key, const void *base, size_t nel, size_t width, int (*compar)(const void *, const void *))
 {
     void *ret_val;
 
