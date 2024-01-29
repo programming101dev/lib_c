5120b5b0e9a80fef1663c58353b76b4ee3f13428
programming101dev <113087924+programming101dev@users.noreply.github.com>
2024-01-28 22:24:33 -0800

    fix warnings

--- src/string.c
+++ src/string.c
@@ -138,10 +138,14 @@ char *p101_strerror(const struct p101_env *env, struct p101_error *err, int errn
     if(copy != NULL && p101_error_has_no_error(err))
     {
         p101_strncpy(env, copy, ret_val, len);
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#ifdef __clang__
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#endif
         copy[len] = '\0';
-#pragma clang diagnostic pop
+#ifdef __clang__
+    #pragma clang diagnostic pop
+#endif
         ret_val = copy;
     }
 
