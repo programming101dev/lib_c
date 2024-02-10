62639ee0eb24ca035ee8ce1e4274128792393493
programming101dev <113087924+programming101dev@users.noreply.github.com>
2024-02-10 08:28:43 -0800

    updated copyright

--- src/inttypes.c
+++ src/inttypes.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 D'Arcy Smith.
+ * Copyright 2021-2024 D'Arcy Smith.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,21 +46,36 @@ imaxdiv_t p101_imaxdiv(const struct p101_env *env, intmax_t numer, intmax_t deno
 intmax_t p101_strtoimax(const struct p101_env *env, struct p101_error *err, const char *restrict nptr, char **restrict endptr, int base)
 {
     intmax_t value;
+    char *temp_endptr;
 
     P101_TRACE(env);
     errno = 0;
-    value = strtoimax(nptr, endptr, base);
+    value = strtoimax(nptr, &temp_endptr, base);
 
-    if(value == 0 || value == INTMAX_MIN || value == INTMAX_MAX)
+    if(value == 0 && errno != 0)
     {
-        if(errno != 0)
-        {
-            P101_ERROR_RAISE_ERRNO(err, errno);
-        }
+        // Error condition: no digits found
+        P101_ERROR_RAISE_ERRNO(err, errno);
+    }
+    else if (value == INTMAX_MIN && errno == ERANGE)
+    {
+        // Error condition: underflow
+        P101_ERROR_RAISE_ERRNO(err, errno);
+    }
+    else if (value == INTMAX_MAX && errno == ERANGE)
+    {
+        // Error condition: overflow
+        P101_ERROR_RAISE_ERRNO(err, errno);
+    }
+
+    if(endptr != NULL)
+    {
+        *endptr = temp_endptr;
     }
 
     return value;
 }
+
 
 uintmax_t p101_strtoumax(const struct p101_env *env, struct p101_error *err, const char *restrict nptr, char **restrict endptr, int base)
 {
