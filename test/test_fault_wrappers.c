36ef444bda07dd3297f2ba009129a0827020a186
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 00:46:00 -0700

    Regenerate portable wrapper fixtures

--- test/test_fault_wrappers.c
+++ test/test_fault_wrappers.c
@@ -7,6 +7,7 @@
 #include <ftw.h>
 #include <limits.h>
 #include <math.h>
+#include <netinet/in.h>
 #include <p101_c/p101_complex.h>
 #include <p101_c/p101_ctype.h>
 #include <p101_c/p101_fenv.h>
@@ -112,10 +113,10 @@ static void native_void_callback(void)
 #define P101_NATIVE_FORMAT_PID_PATH_OR_SKIP(buffer, format)                                                                                                                                                                                                        \
     do                                                                                                                                                                                                                                                             \
     {                                                                                                                                                                                                                                                              \
-        int p101_format_length_;                                                                                                                                                                                                                                   \
+        bool p101_format_ok_;                                                                                                                                                                                                                                      \
                                                                                                                                                                                                                                                                    \
-        p101_format_length_ = snprintf((buffer), sizeof(buffer), (format), (long)getpid());                                                                                                                                                                        \
-        if(p101_format_length_ < 0 || (size_t)p101_format_length_ >= sizeof(buffer))                                                                                                                                                                               \
+        p101_format_ok_ = native_format_pid_path((buffer), sizeof(buffer), (format));                                                                                                                                                                              \
+        if(!p101_format_ok_)                                                                                                                                                                                                                                       \
         {                                                                                                                                                                                                                                                          \
             fprintf(stderr, "native setup failed: path formatting\n");                                                                                                                                                                                             \
             native_child_status = 77;                                                                                                                                                                                                                              \
